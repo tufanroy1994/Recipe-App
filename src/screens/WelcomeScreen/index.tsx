f39189cd1c3f7d59ea8b85ef2d50c5600b1aa92e
Tufan Roy <tufan.roy@omnimindsconsulting.com>
2025-08-24 15:53:54 +0530

    second commit

---
+++ src/screens/WelcomeScreen/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { styles } from './styles';
+
+const WelcomeScreen = () => {
+  return (
+    <View>
+      <Text>Welcome Screen</Text>
+    </View>
+  );
+};
+
+export default WelcomeScreen;
